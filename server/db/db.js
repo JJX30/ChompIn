8b657978378ca649139a852c2c830a408505902e
Mauricio <mauriciodelcas30@gmail.com>
2025-04-21 02:32:00 -0400

    new commit 2:31

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,9 @@ const { Pool } = require('pg');
 // Create a connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Test the connection
