40d4d9c1430daeb7b45e2dfaf42b0f85fabb19a9
Mauricio <mauriciodelcas30@gmail.com>
2025-04-21 02:09:19 -0400

    prod ssl configuration

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,7 @@ const { Pool } = require('pg');
 // Create a connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
 
 // Test the connection
